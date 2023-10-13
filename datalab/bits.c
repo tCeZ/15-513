66c1b7f1237b5c3ad4447f5ccfbedfd5f0872c6f
tCeZ <tedzhang1995@gmail.com>
2023-10-13 17:40:12 -0400

    add datalab

---
+++ datalab/bits.c
@@ -0,0 +1,244 @@
+/*
+ * CS:APP Data Lab
+ *
+ * Ted Zhang <tedz>
+ *
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ */
+
+/* Instructions to Students:
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code
+  must conform to the following style:
+
+  long Funct(long arg1, long arg2, ...) {
+      // brief description of how your implementation works
+      long var1 = Expr1;
+      ...
+      long varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. (Long) integer constants 0 through 255 (0xFFL), inclusive. You are
+      not allowed to use big constants such as 0xffffffffL.
+  2. Function arguments and local variables (no global variables).
+  3. Local variables of type int and long
+  4. Unary integer operations ! ~
+     - Their arguments can have types int or long
+     - Note that ! always returns int, even if the argument is long
+  5. Binary integer operations & ^ | + << >>
+     - Their arguments can have types int or long
+  6. Casting from int to long and from long to int
+
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting other than between int and long.
+  7. Use any data type other than int or long.  This implies that you
+     cannot use arrays, structs, or unions.
+
+  You may assume that your machine:
+  1. Uses 2s complement representations for int and long.
+  2. Data type int is 32 bits, long is 64.
+  3. Performs right shifts arithmetically.
+  4. Has unpredictable behavior when shifting if the shift amount
+     is less than 0 or greater than 31 (int) or 63 (long)
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  //
+  // pow2plus1 - returns 2^x + 1, where 0 <= x <= 63
+  //
+  long pow2plus1(long x) {
+     // exploit ability of shifts to compute powers of 2
+     // Note that the 'L' indicates a long constant
+     return (1L << x) + 1L;
+  }
+
+  //
+  // pow2plus4 - returns 2^x + 4, where 0 <= x <= 63
+  //
+  long pow2plus4(long x) {
+     // exploit ability of shifts to compute powers of 2
+     long result = (1L << x);
+     result += 4L;
+     return result;
+  }
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to
+     check the legality of your solutions.
+  2. Each function has a maximum number of operations (integer, logical,
+     or comparison) that you are allowed to use for your implementation
+     of the function.  The max operator count is checked by dlc.
+     Note that assignment ('=') is not counted; you may use as many of
+     these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+CAUTION:
+  Do not add an #include of <stdio.h> (or any other C library header)
+  to this file.  C library headers almost always contain constructs
+  that dlc does not understand.  For debugging, you can use printf,
+  which is declared for you just below.  It is normally bad practice
+  to declare C library functions by hand, but in this case it's less
+  trouble than any alternative.
+
+  dlc will consider each call to printf to be a violation of the
+  coding style (function calls, after all, are not allowed) so you
+  must remove all your debugging printf's again before submitting your
+  code or testing it with dlc or the BDD checker.  */
+
+extern int printf(const char *, ...);
+
+/* Edit the functions below.  Good luck!  */
+// 2
+/*
+ * copyLSB - set all bits of result to least significant bit of x
+ *   Examples:
+ *     copyLSB(5L) = 0xFFFFFFFFFFFFFFFFL,
+ *     copyLSB(6L) = 0x0000000000000000L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+long copyLSB(long x) {
+    return (x << 63) >> 63;
+}
+/*
+ * dividePower2 - Compute x/(2^n), for 0 <= n <= 62
+ *  Round toward zero
+ *   Examples: dividePower2(15L,1L) = 7L, dividePower2(-33L,4L) = -2L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+long dividePower2(long x, long n) {
+    
+}
+/*
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (least significant) to 7 (most significant)
+ *   Examples: getByte(0x12345678L,1) = 0x56L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+long getByte(long x, long n) {
+    return 2L;
+}
+/*
+ * anyOddBit - return 1 if any odd-numbered bit in word set to 1
+ *   where bits are numbered from 0 (least significant) to 63 (most significant)
+ *   Examples anyOddBit(0x5L) = 0L, anyOddBit(0x7L) = 1L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 14
+ *   Rating: 2
+ */
+long anyOddBit(long x) {
+    return 2;
+}
+// 3
+/*
+ * conditional - same as x ? y : z
+ *   Example: conditional(2,4L,5L) = 4L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+long conditional(long x, long y, long z) {
+    return 2L;
+}
+/*
+ * subtractionOK - Determine if can compute x-y without overflow
+ *   Example: subtractionOK(0x8000000000000000L,0x8000000000000000L) = 1L,
+ *            subtractionOK(0x8000000000000000L,0x7000000000000000L) = 0L,
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+long subtractionOK(long x, long y) {
+    return 2L;
+}
+/*
+ * bitMask - Generate a mask consisting of all 1's
+ *   between lowbit and highbit
+ *   Examples: bitMask(5L,3L) = 0x38L
+ *   Assume 0 <= lowbit < 64, and 0 <= highbit < 64
+ *   If lowbit > highbit, then mask should be all 0's
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+long bitMask(long highbit, long lowbit) {
+    return 2L;
+}
+/*
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 63
+ *   Examples:
+ *      rotateLeft(0x8765432187654321L,4L) = 0x7654321876543218L
+ *   Legal ops: ~ & ^ | + << >> !
+ *   Max ops: 25
+ *   Rating: 3
+ */
+long rotateLeft(long x, long n) {
+    return 2;
+}
+// 4
+/*
+ * trueFiveEighths - multiplies by 5/8 rounding toward 0,
+ *  avoiding errors due to overflow
+ *  Examples:
+ *    trueFiveEighths(11L) = 6L
+ *    trueFiveEighths(-9L) = -5L
+ *    trueFiveEighths(0x3000000000000000L) = 0x1E00000000000000L (no overflow)
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 20
+ *  Rating: 4
+ */
+long trueFiveEighths(long x) {
+    return 2L;
+}
+/*
+ * bitCount - returns count of number of 1's in word
+ *   Examples: bitCount(5L) = 2, bitCount(7L) = 3
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 50
+ *   Rating: 4
+ */
+long bitCount(long x) {
+    return 2;
+}
+/*
+ * bitParity - returns 1 if x contains an odd number of 0's
+ *   Examples: bitParity(5L) = 0L, bitParity(7L) = 1L
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 22
+ *   Rating: 4
+ */
+long bitParity(long x) {
+    return 2L;
+}
---
+++ datalab/bits.h
@@ -0,0 +1,25 @@
+//2
+long copyLSB(long);
+long test_copyLSB(long);
+long dividePower2(long, long);
+long test_dividePower2(long, long);
+long getByte(long, long);
+long test_getByte(long, long);
+long anyOddBit(long);
+long test_anyOddBit(long);
+//3
+long conditional(long, long, long);
+long test_conditional(long, long, long);
+long subtractionOK(long, long);
+long test_subtractionOK(long, long);
+long bitMask(long, long);
+long test_bitMask(long, long);
+long rotateLeft(long, long);
+long test_rotateLeft(long, long);
+//4
+long trueFiveEighths(long);
+long test_trueFiveEighths(long);
+long bitCount(long);
+long test_bitCount(long);
+long bitParity(long);
+long test_bitParity(long);
